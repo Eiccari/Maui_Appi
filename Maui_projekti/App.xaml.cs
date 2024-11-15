706ca30a74263a7429c590e8d11c89093eb46cac
Eiccari <p32157@edu.sasky.fi>
2024-11-15 14:54:18 +0200

    Add project files.

---
+++ Maui_projekti/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui_projekti
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Maui_projekti/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui_projekti
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
